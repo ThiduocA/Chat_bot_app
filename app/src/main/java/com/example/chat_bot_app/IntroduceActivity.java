468d1691d01c3f86bce7bbdb857b3104d5510a48
Hwngne <Xhung2k3@gmail.com>
2024-08-04 15:29:35 +0700

    Code main activity

--- app/src/main/java/com/example/chat_bot_app/IntroduceActivity.java
+++ app/src/main/java/com/example/chat_bot_app/IntroduceActivity.java
@@ -1,6 +1,9 @@
 package com.example.chat_bot_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class IntroduceActivity extends AppCompatActivity {
-
+private Button btn_continue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class IntroduceActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn_continue = findViewById(R.id.button);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroduceActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
