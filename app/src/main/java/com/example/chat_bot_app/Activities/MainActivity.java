91731d9cc8ecb1087da43b92d6671e058d8a249b
Hwngne <Xhung2k3@gmail.com>
2024-08-06 20:17:59 +0700

    Edit recycleview explain, write & edit, translate

--- app/src/main/java/com/example/chat_bot_app/Activities/MainActivity.java
+++ app/src/main/java/com/example/chat_bot_app/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.chat_bot_app.Activities;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,17 +31,48 @@ private RecyclerView recyclerView;
             return insets;
         });
 
-        initRecyclerview();
+        initRecyclerviewExplain();
+        initRecyclerviewWrite();
+        initRecyclerviewTranslate();
+
+        EditText editText = findViewById(R.id.editTextText);
+        editText.setBackgroundResource(0);
     }
 
-    private void initRecyclerview() {
+    private void initRecyclerviewTranslate() {
+        ArrayList<Question> questions = new ArrayList<>();
+
+        questions.add(new Question("How do you say “how are you” in korean?"));
+        questions.add(new Question("Write a poem about flower and love"));
+
+        recyclerView = findViewById(R.id.translate);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        adapterQuestion = new QuestionAdapter(questions);
+        recyclerView.setAdapter(adapterQuestion);
+    }
+
+    private void initRecyclerviewWrite() {
         ArrayList<Question> questions = new ArrayList<>();
 
         questions.add(new Question("Explain Quantum physics"));
         questions.add(new Question("What are wormholes explain like i am 5"));
-        questions.add(new Question("What are wormholes explain like i am 5"));
 
         recyclerView = findViewById(R.id.explain);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        adapterQuestion = new QuestionAdapter(questions);
+        recyclerView.setAdapter(adapterQuestion);
+    }
+
+    private void initRecyclerviewExplain() {
+        ArrayList<Question> questions = new ArrayList<>();
+
+        questions.add(new Question("Write a tweet about global warming"));
+        questions.add(new Question("Write a poem about flower and love"));
+        questions.add(new Question("Write a rap song lyrics about"));
+
+        recyclerView = findViewById(R.id.write);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         adapterQuestion = new QuestionAdapter(questions);
