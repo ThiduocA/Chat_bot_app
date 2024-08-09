7a903569da7c896cd9a2292d0e8b57edb005b1d7
Hwngne <Xhung2k3@gmail.com>
2024-08-09 20:30:37 +0700

    Fix send message function

--- app/src/main/java/com/example/chat_bot_app/Activities/MainActivity.java
+++ app/src/main/java/com/example/chat_bot_app/Activities/MainActivity.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 private RecyclerView.Adapter adapterQuestion;
-private RecyclerView recyclerView;
+private RecyclerView recyclerView, recyclerViewChat;
 private ImageView sendBtn;
 private EditText messageEditText;
 List<Message> messageList;
@@ -40,19 +40,20 @@ MessageAdapter messageAdapter;
             return insets;
         });
 
-        initRecyclerviewExplain();
-        initRecyclerviewWrite();
-        initRecyclerviewTranslate();
+//        initRecyclerviewExplain();
+//        initRecyclerviewWrite();
+//        initRecyclerviewTranslate();
 
         EditText editText = findViewById(R.id.editTextText);
         editText.setBackgroundResource(0);
 
         //setup recycler view
         messageAdapter = new MessageAdapter(messageList);
-        recyclerView.setAdapter(messageAdapter);
+        recyclerViewChat = findViewById(R.id.recycleView);
+        recyclerViewChat.setAdapter(messageAdapter);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setStackFromEnd(true);
-        recyclerView.setLayoutManager(llm);
+        recyclerViewChat.setLayoutManager(llm);
 
         clickBtn();
     }
@@ -64,49 +65,51 @@ MessageAdapter messageAdapter;
             String question = messageEditText.getText().toString().trim();
             //Toast.makeText(this, question, Toast.LENGTH_LONG).show();
             addToChat(question, Message.SENT_BY_ME);
+            messageEditText.setText("");
+
         }));
     }
 
-    private void initRecyclerviewTranslate() {
-        ArrayList<Question> questions = new ArrayList<>();
-
-        questions.add(new Question("How do you say “how are you” in korean?"));
-        questions.add(new Question("Write a poem about flower and love"));
-
-        recyclerView = findViewById(R.id.translate);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-
-        adapterQuestion = new QuestionAdapter(questions);
-        recyclerView.setAdapter(adapterQuestion);
-    }
-
-    private void initRecyclerviewWrite() {
-        ArrayList<Question> questions = new ArrayList<>();
-
-        questions.add(new Question("Explain Quantum physics"));
-        questions.add(new Question("What are wormholes explain like i am 5"));
-
-        recyclerView = findViewById(R.id.explain);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-
-        adapterQuestion = new QuestionAdapter(questions);
-        recyclerView.setAdapter(adapterQuestion);
-    }
-
-    private void initRecyclerviewExplain() {
-        ArrayList<Question> questions = new ArrayList<>();
-
-        questions.add(new Question("Write a tweet about global warming"));
-        questions.add(new Question("Write a poem about flower and love"));
-        questions.add(new Question("Write a rap song lyrics about"));
-
-        recyclerView = findViewById(R.id.write);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-
-        adapterQuestion = new QuestionAdapter(questions);
-        recyclerView.setAdapter(adapterQuestion);
-
-    }
+//    private void initRecyclerviewTranslate() {
+//        ArrayList<Question> questions = new ArrayList<>();
+//
+//        questions.add(new Question("How do you say “how are you” in korean?"));
+//        questions.add(new Question("Write a poem about flower and love"));
+//
+//        recyclerView = findViewById(R.id.translate);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//
+//        adapterQuestion = new QuestionAdapter(questions);
+//        recyclerView.setAdapter(adapterQuestion);
+//    }
+//
+//    private void initRecyclerviewWrite() {
+//        ArrayList<Question> questions = new ArrayList<>();
+//
+//        questions.add(new Question("Explain Quantum physics"));
+//        questions.add(new Question("What are wormholes explain like i am 5"));
+//
+//        recyclerView = findViewById(R.id.explain);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//
+//        adapterQuestion = new QuestionAdapter(questions);
+//        recyclerView.setAdapter(adapterQuestion);
+//    }
+//
+//    private void initRecyclerviewExplain() {
+//        ArrayList<Question> questions = new ArrayList<>();
+//
+//        questions.add(new Question("Write a tweet about global warming"));
+//        questions.add(new Question("Write a poem about flower and love"));
+//        questions.add(new Question("Write a rap song lyrics about"));
+//
+//        recyclerView = findViewById(R.id.write);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//
+//        adapterQuestion = new QuestionAdapter(questions);
+//        recyclerView.setAdapter(adapterQuestion);
+//
+//    }
 
     private void addToChat(String message, String sentBy){
         runOnUiThread(new Runnable() {
@@ -114,7 +117,7 @@ MessageAdapter messageAdapter;
             public void run() {
                 messageList.add(new Message(message, sentBy));
                 messageAdapter.notifyDataSetChanged();
-                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+                recyclerViewChat.smoothScrollToPosition(messageAdapter.getItemCount());
             }
         });
 
